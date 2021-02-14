9261e3bd4dfe4c68b77bfdc6494f08b179dcbfad
Dan Aument <danaument@gmail.com>
2021-02-14 14:30:48 -0600

    schema update

--- models/workout.js
+++ models/workout.js
@@ -5,32 +5,34 @@ const Schema = mongoose.Schema;
 const workoutSchema = new Schema({
   day: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
-  exercises: [{
+  exercises: [
+    {
       type: {
-          type: String,
-          trim: true,
-          required: "Enter a type for exercise"
+        type: String,
+        trim: true,
+        required: "Enter a type for exercise",
       },
       name: {
-          type: String,
-          trim: true,
-          required: "Enter a name for the exercise"
-      }, 
+        type: String,
+        trim: true,
+        required: "Enter a name for the exercise",
+      },
       duration: {
-          type: Number
+        type: Number,
       },
       weight: {
-          type: Number
+        type: Number,
       },
       reps: {
-          type: Number
+        type: Number,
       },
       sets: {
-          type: Number
-      }
-  }]
+        type: Number,
+      },
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);

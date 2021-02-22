7476679221dca96fac7e92dd70e974997afd4150
Dan Aument <danaument@gmail.com>
2021-02-22 12:29:03 -0600

    fixed route workout vs workouts, worked on api structure

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,10 +2,11 @@ const router = require("express").Router();
 const workout = require("../models/workout.js");
 
 router.put("/api/workouts/:id", ({ body }, res) => {
+  console.log(body);
   workout
     .create(body)
     .then(({ _id }) =>
-      workout.findOneAndUpdate({}, { $push: { workout: _id } }, { new: true })
+      workout.findOneAndUpdate({_id: req.params.id}, { $push: { workout: _id } }, { new: true })
     )
     .then((workout) => {
       res.json(workout);
@@ -15,19 +16,30 @@ router.put("/api/workouts/:id", ({ body }, res) => {
     });
 });
 
-// router.post("/api/workout/bulk", ({ body }, res) => {
-//   workout.insertMany(body)
-//     .then(dbworkout => {
-//       res.json(dbworkout);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
+router.post("/api/workouts/", ({ body }, res) => {
+  workout.create(body)
+    .then(dbworkout => {
+      res.json(dbworkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  workout.find({})
+  .then((dbworkout) => {
+    res.json(dbworkout);
+  })
+  .catch((err) => {
+    res.status(400).json(err);
+  });
+})
 
 router.get("/api/workouts", (req, res) => {
   workout
     .find({})
+    // .limit(1)  //it looks like the front end expects all the work outs and then only returns json[json.length - 1]
     .sort({ date: -1 })
     .then((dbworkout) => {
       res.json(dbworkout);
